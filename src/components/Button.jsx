e2a6b06f4091cbeabe112f4c575a920f07156ae0
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-15 21:48:58 +0530

    Updated buttons with framer motion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
-
+import { motion } from 'framer-motion';
 const Button = ({ filterItem, setItem, menuItems, data }) => {
     return (
         <>
             <div className="d-flex justify-content-center">
                 {menuItems.map((Val, id) => {
                     return (
-                        <button
-                            className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
-                            onClick={() => filterItem(Val)}
-                            key={id}
-                        >
-                            {Val}
-                        </button>
+                        <>
+                            <motion.a
+                                className="box"
+                                whileHover={{ scale: 1.2 }}
+                                whileTap={{ scale: 0.9 }}
+                                transition={{
+                                    type: 'spring',
+                                    stiffness: 400,
+                                    damping: 17,
+                                }}
+                                onClick={() => filterItem(Val)}
+                                key={id}
+                            >
+                                {Val}
+                            </motion.a>
+                        </>
                     );
                 })}
-                <button
-                    className="btn-dark text-white p-1 px-3 mx-5 fw-bold btn"
+                <motion.a
+                    className="box"
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 17,
+                    }}
                     onClick={() => setItem(data)}
                 >
                     All
-                </button>
+                </motion.a>
             </div>
         </>
     );
 };
 
 export default Button;
+{
+    /* <button onClick={() => setItem(data)}>
+All
+</button> */
+}
