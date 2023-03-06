59c0486e463f2eabf4830590a3f4660a361c5d5c
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-03-06 20:29:05 +0530

    Updated to v3.1

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,7 +3,7 @@ import github from '../assets/github.png';
 const Showcase = () => {
     return (
         <div className="row">
-            <div className="col-lg-12 d-flex justify-content-center align-items-center">
+            <div className="col-lg-12 py-3 d-flex justify-content-center align-items-center">
                 <img className="w-50 github" src={github} alt="github" />
             </div>
         </div>
